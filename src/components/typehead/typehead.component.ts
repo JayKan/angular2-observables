ddc4905c29ab56a2b241570423ee2b5625aaa692
Jay Kan <leicasper@gmail.com>
2016-04-01 13:17:08 -0700

    example(typehead): add typehead component to show async observables with Youtube API

--- src/components/typehead/typehead.component.ts
+++ src/components/typehead/typehead.component.ts
@@ -1,13 +1,48 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
+import { Control } from 'angular2/common';
+import { Observable } from 'rxjs/Observable';
+import { Http } from 'angular2/http';
+import 'rxjs/Rx';
+
+const BASE_URL: string = 'https://www.googleapis.com/youtube/v3/search';
+const API_TOKEN: string = 'AIzaSyAJk1xUI72YYfBMgEc84gjHUX-k2AN6-B0';
+const makeURL = (query): string => `${BASE_URL}?q=${query}&part=snippet&key=${API_TOKEN}`;
 
 @Component({
-  selector: 'typehead',
+  selector: 'typehead-demo',
+  styles: [`
+    .typehead-demo p {
+       margin-bottom: 5px;
+    }
+  `],
   template: `
-    <div>
-      <p>Typehead example</p>
+    <div class="typehead-demo">
+      <p>Search youtube videos:</p>        
+      <input type="text" class="form-control" [ngFormControl]="searchInput">
+      <ul>
+        <li *ngFor="#video of videos | async">{{ video | json }}</li>
+      </ul>
     </div>
   `
 })
-export class TypeHeadComponent {
-  constructor() {}
+
+export class TypeHeadComponent implements OnInit {
+
+  searchInput: Control = new Control('');
+  videos: Observable<any>;
+
+  constructor(private _http: Http) {}
+
+  ngOnInit(): void {
+    this.videos = this.searchInput.valueChanges
+        .debounceTime(200) // optionally debounce
+        .map(inputText => makeURL(inputText))
+        //flatMap won't cancel in-flight requests, switchMap will...
+        //.flatMap(url => http.get(url).map(res => res.json()))
+        .switchMap(url => this._http.get(url).map(res => res.json()))
+        .map(response => response.items);
+
+    console.log('Videos: ', this.videos);
+
+  }
 }
--- src/demo-app/demo-app.ts
+++ src/demo-app/demo-app.ts
@@ -1,13 +1,13 @@
 import { Component, ViewEncapsulation} from 'angular2/core';
 import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig } from 'angular2/router';
+import { HTTP_PROVIDERS } from 'angular2/http';
 import { TypeHeadComponent } from '../components/typehead/typehead';
-
 
 @Component({
   selector: 'demo-app',
   templateUrl: 'demo-app/demo-app.component.html',
   directives: [ROUTER_DIRECTIVES],
-  providers: [ROUTER_PROVIDERS],
+  providers: [ROUTER_PROVIDERS, HTTP_PROVIDERS],
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['demo-app/demo-app.component.css']
 })
