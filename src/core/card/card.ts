c65b95caa4f849ab667a83750f27e56d3894b4cf
Jay Kan <leicasper@gmail.com>
2016-04-04 13:49:08 -0700

    chore(): add common shared card component

---
+++ src/core/card/card.ts
@@ -0,0 +1,7 @@
+import { CONST_EXPR } from 'angular2/src/facade/lang';
+import { VideoCard } from './video-card';
+import { VideoCardHeader } from './video-card-header';
+import { VideoCardTitleGroup } from './video-card-title-group';
+
+export const VIDEO_CARD_DIRECTIVES: any[] = CONST_EXPR([VideoCard, VideoCardHeader, VideoCardTitleGroup]);
+
