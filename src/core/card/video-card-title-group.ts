b26b6fd551b2dbfd1a264ed6a567260cd217717e
Jay Kan <leicasper@gmail.com>
2016-04-05 11:38:32 -0700

    feat(video-card-title-group): add common/shared video-card-title-group component

--- src/core/card/video-card-title-group.ts
+++ src/core/card/video-card-title-group.ts
@@ -1,3 +1,22 @@
-export class VideoCardTitleGroup {
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, OnInit } from 'angular2/core';
+
+@Component({
+  selector: 'video-card-title-group',
+  template: `
+  <div>
+    <ng-content select="video-card-title, video-card-subtitle"></ng-content>
+  </div>
+  <ng-content select="img"></ng-content>
+  <ng-content></ng-content>
+  `,
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class VideoCardTitleGroup implements OnInit {
+  
+  ngOnInit(): void {
+    
+  }
   
 }
