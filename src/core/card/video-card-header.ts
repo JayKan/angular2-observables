43e2078ad9e87a282de64ff4dc53c895a6435067
Jay Kan <leicasper@gmail.com>
2016-04-05 11:35:38 -0700

    feat(video-card-header): add common/shared video-card-header component

--- src/core/card/video-card-header.ts
+++ src/core/card/video-card-header.ts
@@ -1,3 +1,21 @@
-export class VideoCardHeader {
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, OnInit } from 'angular2/core';
+
+@Component({
+  selector: 'video-card-header',
+  template: `
+  <ng-content select="[video-card-avatar]"></ng-content>
+  <div class="video-card-header-text">
+    <ng-content select="video-card-title, video-card-subtitle"></ng-content>
+  </div>
+  <ng-content></ng-content>  
+  `,
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class VideoCardHeader implements OnInit {
+  ngOnInit(): void {
+    console.log('---------- Video-card-header Init() ----------');
+  }
   
 }
