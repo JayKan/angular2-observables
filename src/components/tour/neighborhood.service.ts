f39e28ad92fcce25f351643f2b054732d24db65b
Jay Kan <leicasper@gmail.com>
2016-04-25 13:31:20 -0700

    feat(tour): add neighbhood observable serivce

---
+++ src/components/tour/neighborhood.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from 'angular2/core';
+import { Neighborhood } from './neighborhood.model';
+import { Observable, Subject } from 'rxjs/Rx';
+// import { SocketService } from './socket.service';
+
+interface INeighborhoodsOperation extends Function {
+  (neighborhoods: Neighborhood[]): Neighborhood[]  
+}
+
+@Injectable()
+export class NeighborhoodService {
+
+  neighborhoods: Neighborhood[];
+  $neighborhoods: Observable<Neighborhood[]>;
+
+  $$newNeighborhoods: Subject<any> = new Subject<any>();
+  $$create:  Subject<any> = new Subject<any>();
+  $$updates: Subject<any> = new Subject<any>();
+
+  constructor() {
+    this.neighborhoods = [];
+    
+    this.$neighborhoods = this.$$updates
+        .scan((neighborhoods: Neighborhood[],
+              operation: INeighborhoodsOperation) => {
+          return operation(neighborhoods);
+        }, [])
+        .publishReplay(1)
+        .refCount();
+    
+    this.$$create
+        .map((neighborhood: Neighborhood) => {
+          return (neighborhoods: Neighborhood[]) => {
+            return neighborhoods.concat(neighborhood);
+          };
+        })
+        .subscribe(this.$$updates);
+    
+    this.$$newNeighborhoods.subscribe(this.$$create);
+
+    // this.socketAPI.onUpdateNeighborhood((name, amount) => {
+    //   let neighborhood: Neighborhood = this.neighborhoods.filter(
+    //       (n) => n.name == name
+    //   )[0];
+    //
+    //   if (neighborhood) {
+    //     neighborhood.setTTAmount(amount);
+    //   }
+    // })
+  }
+
+
+  pushNeighborhood(...names: string[]): void {
+    // console.log('Names: ', typeof names, names);
+    // using push instead of concat because concat()
+    // constructs a new array and we don't want that.
+    // also push() can push multiple elements (...)
+    this.neighborhoods.push(
+        ...names
+            .map(name => new Neighborhood(name))
+    );
+  }
+
+  add(neighborhood: Neighborhood): void {
+    this.$$newNeighborhoods.next(neighborhood);
+  }
+  
+  
+  // updateNeighborhood(neighborhood: Neighborhood, amount: number) {
+  //   this.socketAPI.updateNeighborhood(neighborhood.name, amount);
+  // }
+}
---
+++ src/components/tour/socket.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from 'angular2/core';
+
+@Injectable()
+export class SocketService {
+  socket: SocketIOClient.Socket;
+
+  constructor() {
+    this.socket = io.connect('http://localhost:3000');
+  }
+
+  /**
+   * @method updateNeighborhood
+   * @param name {string}
+   * @param TTAmount {number}
+   * @description Take a change from current application
+   * and propagate to others who are running the apps.
+   */
+  updateNeighborhood(name: string, TTAmount: number): void {
+    this.socket.emit('updateNeighborhoods', {
+      // dynamically computed property [name]
+      [name]: TTAmount
+    });
+  }
+
+  /**
+   * @method updateNeighborhood
+   * @param listener {Function}
+   * @description When others update our app, we will have a listener be called with
+   * the neighborhood name and the amount.
+   */
+  onUpdateNeighborhood(listener: Function): void {
+    this.socket.on('updateNeighborhoods', data => {
+      console.log('socketAPI(): ', data);
+      Object.keys(data).forEach(
+        (name) => listener(name, data[name])
+      );
+    });
+  }
+
+}
