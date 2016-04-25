faa78e8b4873952d460761aee89d6ebda8c1b951
Jay Kan <leicasper@gmail.com>
2016-04-25 14:45:59 -0700

    refactor(demo-app): add tourist route

--- src/demo-app/demo-app.ts
+++ src/demo-app/demo-app.ts
@@ -2,6 +2,7 @@ import { Component, ViewEncapsulation} from 'angular2/core';
 import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig } from 'angular2/router';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import { TypeHeadComponent } from '../components/typehead/typehead';
+import { TouristComponent } from '../components/tour/tour';
 
 @Component({
   selector: 'demo-app',
@@ -14,13 +15,15 @@ import { TypeHeadComponent } from '../components/typehead/typehead';
 
 @RouteConfig([
   { path: '/typehead', name: 'Typehead', component: TypeHeadComponent, useAsDefault: true },
+  { path: '/tourist',  name: 'Tourist',  component: TouristComponent                      }
 ])
 
 export class AppComponent {
   menuItems: Array<any>;
+  
   constructor() {
     this.menuItems = [
-      { caption: 'Typehead', link: ['Typehead'] },
+      {caption: 'Typehead', link: ['Typehead']},
     ]
   }
 }
