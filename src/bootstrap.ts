9ff9167de5b08abd6d07264f607b83e5d3ad78b6
Jay Kan <leicasper@gmail.com>
2016-07-18 12:04:46 -0700

    refactor: upgrade to new angular2 router implementation

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,7 +1,21 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './demo-app/demo-app';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { APP_ROUTER_PROVIDERS } from './app.routes';
 
-bootstrap(AppComponent, [])
-  .then(success => { console.log('Bootstrap success') })
-  .catch(error => console.error(error))
-;
+// Extend Observable throughout the app
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/observable/of';
+import 'rxjs/observable/interval';
+
+bootstrap(AppComponent, [
+  HTTP_PROVIDERS,
+  APP_ROUTER_PROVIDERS
+])
+.then(success => { console.log('Bootstrap success') })
+.catch(error => console.error(error));
